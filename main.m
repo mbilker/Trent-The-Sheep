cc9d71f353c1c726b99c2bca293a815739d81ce2
Matt Bilker <me@mbilker.us>
2012-03-21 00:31:55 -0400

    Ok

---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Perm and Comb
+//
+//  Created by mbilker on 12/9/11.
+//  Copyright mbilker 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+	NSAutoreleasePool *pool = [NSAutoreleasePool new];
+	int retVal = UIApplicationMain(argc, argv, nil, @"Perm_and_CombAppDelegate");
+	[pool release];
+	return retVal;
+}
